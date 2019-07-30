dfdaa6b0e02b5060e73d5cd41eb333cf5eedac83
vsnyc <vinod.k.shukla@gmail.com>
2019-07-29 23:04:53 -0400

    removed deploy script per feedback

--- samples/session-manager-ssh/deploy-template.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export KEY_PAIR_NAME=cikey
-aws cloudformation --region us-east-2 create-stack --stack-name ssm-ssh-demo \
-  --template-url https://aws-quickstart.s3.amazonaws.com/quickstart-examples/samples/session-manager-ssh/session-manager-ssh-example.yaml \
-  --parameters ParameterKey=KeyPairName,ParameterValue=${KEY_PAIR_NAME} ParameterKey=AvailabilityZones,ParameterValue=us-east-2a\\,us-east-2b \
-  --capabilities "CAPABILITY_IAM" --disable-rollback
